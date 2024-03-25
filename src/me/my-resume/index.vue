c8284ac377d9d3defdc206d5816376dc9380e9d5
姚黎明 <yaoliming@kjteck.com>
2024-03-25 09:13:02 +0800

    feat: 提交

---
+++ src/me/my-resume/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="my-resume">
+        <div class="my-resume-hed">测试提交</div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
